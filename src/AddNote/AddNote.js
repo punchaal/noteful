668f1b48766b0012d0a287edb7f4fdab5c4bdf7e
Pratik <punchaal@gmail.com>
2019-06-11 08:04:20 -0400

    Added error boundary and validation

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -1,84 +1,161 @@
-import React, { Component } from 'react'
-import NotefulForm from '../NotefulForm/NotefulForm'
-import ApiContext from '../ApiContext'
-import config from '../config'
-import './AddNote.css'
+import React, { Component } from 'react';
+import NotefulForm from '../NotefulForm/NotefulForm';
+import NoteContext from '../NoteContext';
+import './AddNote.css';
 
 export default class AddNote extends Component {
-  static defaultProps = {
-    history: {
-      push: () => { }
-    },
+  constructor() {
+    super();
+    this.state = {
+      error: null,
+      name: '',
+      content: '',
+      id: '',
+      nameValid: false,
+      idValid: false,
+      validationMessage: ''
+    };
   }
-  static contextType = ApiContext;
+  static contextType = NoteContext;
+  static defaultProps = {
+    folders: []
+  };
 
-  handleSubmit = e => {
-    e.preventDefault()
-    const newNote = {
-      name: e.target['note-name'].value,
-      content: e.target['note-content'].value,
-      folderId: e.target['note-folder-id'].value,
-      modified: new Date(),
+  isNameValid = event => {
+    event.preventDefault();
+    if (!this.state.name) {
+      this.setState({
+        validationMessage: 'Note name can not be blank.',
+        nameValid: false
+      });
+    } else if (!this.state.id) {
+      this.setState({
+        validationMessage: 'You must choose a valid folder.',
+        idValid: false
+      });
+    } else {
+      this.setState(
+        {
+          validationMessage: '',
+          nameValid: true
+        },
+        () => {
+          this.handleAddNote();
+        }
+      );
     }
-    fetch(`${config.API_ENDPOINT}/notes`, {
+  };
+
+  handleAddNote = () => {
+    const options = {
       method: 'POST',
       headers: {
-        'content-type': 'application/json'
+        'Content-Type': 'application/json',
+        'Authorization':'bearer b3f3a2f8-7be2-11e9-8f9e-2a86e4085a59'
       },
-      body: JSON.stringify(newNote),
-    })
+      body: JSON.stringify({
+        name: this.state.name,
+        modified: new Date(),
+        folderId: this.state.id,
+        content: this.state.content
+      })
+    };
+
+    fetch('http://localhost:8000/api/notes', options)
       .then(res => {
-        if (!res.ok)
-          return res.json().then(e => Promise.reject(e))
-        return res.json()
+        if (!res.ok) {
+          throw new Error('Something went wrong');
+        }
+        return res;
       })
-      .then(note => {
-        this.context.addNote(note)
-        this.props.history.push(`/folder/${note.folderId}`)
+      .then(res => res.json())
+      .then(data => {
+        this.context.handleAddNote(data);
       })
-      .catch(error => {
-        console.error({ error })
-      })
-  }
+      .catch(err => {
+        this.setState({ error: err.message });
+      });
+  };
+
+  nameChange = letter => {
+    this.setState({ name: letter });
+  };
+
+  contentChange = letter => {
+    this.setState({ content: letter });
+  };
+
+  idChange = letter => {
+    this.setState({ id: letter });
+  };
 
   render() {
-    const { folders=[] } = this.context
     return (
       <section className='AddNote'>
         <h2>Create a note</h2>
-        <NotefulForm onSubmit={this.handleSubmit}>
+        <NotefulForm
+          onSubmit={event => {
+            this.isNameValid(event);
+          }}
+        >
           <div className='field'>
-            <label htmlFor='note-name-input'>
-              Name
-            </label>
-            <input type='text' id='note-name-input' name='note-name' />
+            <label htmlFor='note-name-input'>Name</label>
+            <input
+              type='text'
+              id='note-name-input'
+              name='note'
+              onChange={event => {
+                this.nameChange(event.target.value);
+              }}
+            />
+          </div>
+          {!this.state.nameValid && (
+            <div>
+              <p>{this.state.validationMessage}</p>
+            </div>
+          )}
+          <div className='field'>
+            <label htmlFor='note-content-input'>Content</label>
+            <textarea
+              id='note-content-input'
+              name='content'
+              onChange={event => {
+                this.contentChange(event.target.value);
+              }}
+            />
           </div>
           <div className='field'>
-            <label htmlFor='note-content-input'>
-              Content
-            </label>
-            <textarea id='note-content-input' name='note-content' />
-          </div>
-          <div className='field'>
-            <label htmlFor='note-folder-select'>
-              Folder
-            </label>
-            <select id='note-folder-select' name='note-folder-id'>
+            <label htmlFor='note-folder-select'>Folder</label>
+            <select
+              id='note-folder-select'
+              name='folder'
+              onChange={event => {
+                this.idChange(event.target.value);
+              }}
+            >
               <option value={null}>...</option>
-              {folders.map(folder =>
-                <option key={folder.id} value={folder.id}>
+              {this.context.folders.map(folder => (
+                <option key={folder.name} name='folder' value={folder.id}>
                   {folder.name}
                 </option>
-              )}
+              ))}
             </select>
+            {!this.state.nameValid && (
+              <div>
+                <p>{this.state.validationMessage}</p>
+              </div>
+            )}
           </div>
           <div className='buttons'>
-            <button type='submit'>
-              Add note
-            </button>
+            <button type='submit'>Add note</button>
           </div>
         </NotefulForm>
+        {this.state.error && (
+          <div>
+            <p>{this.state.error}</p>
+          </div>
+        )}
       </section>
-    )
+    );
   }
 }
